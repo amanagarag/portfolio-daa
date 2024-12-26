7ca0429fb70916d00d2e43996dea6d7f3400efe3
amanagarag <144016981+amanagarag@users.noreply.github.com>
2024-12-26 14:17:56 +0530

    Update Dijkstra.cpp

--- Dijkstra_code/Dijkstra.cpp
+++ Dijkstra_code/Dijkstra.cpp
@@ -1,64 +1,79 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <limits.h>
-
+#include <climits>
+#include <map>
 using namespace std;
 
-typedef pair<int, int> pii;
+class TrafficNetwork {
+private:
+    int vertices;
+    map<int, vector<pair<int, int>>> adjList;
 
-void dijkstra(int source, vector<vector<pii>>& graph, vector<int>& distances) {
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-    distances[source] = 0;
-    pq.push({0, source});
-    
-    while (!pq.empty()) {
-        int current_distance = pq.top().first;
-        int current_vertex = pq.top().second;
-        pq.pop();
-        
-        if (current_distance > distances[current_vertex]) {
-            continue;
-        }
-        
-        for (auto& neighbor : graph[current_vertex]) {
-            int neighbor_vertex = neighbor.first;
-            int edge_weight = neighbor.second;
-            int new_distance = current_distance + edge_weight;
-            
-            if (new_distance < distances[neighbor_vertex]) {
-                distances[neighbor_vertex] = new_distance;
-                pq.push({new_distance, neighbor_vertex});
+public:
+    TrafficNetwork(int v) : vertices(v) {}
+
+    void addEdge(int u, int v, int weight) {
+        adjList[u].push_back({v, weight});
+        adjList[v].push_back({u, weight});
+    }
+
+    vector<int> dijkstra(int start) {
+        vector<int> dist(vertices, INT_MAX);
+        dist[start] = 0;
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        pq.push({0, start});
+
+        while (!pq.empty()) {
+            int d = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            for (auto &neighbor : adjList[node]) {
+                int nextNode = neighbor.first;
+                int weight = neighbor.second;
+
+                if (dist[node] + weight < dist[nextNode]) {
+                    dist[nextNode] = dist[node] + weight;
+                    pq.push({dist[nextNode], nextNode});
+                }
             }
         }
+
+        return dist;
     }
-}
+
+    void divideAndConquer(int start, int end) {
+        if (start == end) return;
+
+        int mid = (start + end) / 2;
+        divideAndConquer(start, mid);
+        divideAndConquer(mid + 1, end);
+    }
+};
 
 int main() {
-    int n, m;
-    cout << "Enter the number of vertices and edges: ";
-    cin >> n >> m;
-    
-    vector<vector<pii>> graph(n);
-    
-    cout << "Enter the edges in the format (u, v, weight):\n";
-    for (int i = 0; i < m; ++i) {
-        int u, v, weight;
-        cin >> u >> v >> weight;
-        graph[u].push_back({v, weight});
+    int vertices = 6;
+    TrafficNetwork network(vertices);
+
+    network.addEdge(0, 1, 4);
+    network.addEdge(0, 2, 2);
+    network.addEdge(1, 2, 5);
+    network.addEdge(1, 3, 10);
+    network.addEdge(2, 3, 3);
+    network.addEdge(3, 4, 1);
+    network.addEdge(4, 5, 7);
+
+    int startNode = 0;
+    vector<int> shortestPaths = network.dijkstra(startNode);
+
+    for (int i = 0; i < shortestPaths.size(); ++i) {
+        cout << "To node " << i << ": " << shortestPaths[i] << " minutes\n";
     }
-    
-    int source;
-    cout << "Enter the source vertex: ";
-    cin >> source;
-    
-    vector<int> distances(n, INT_MAX);
-    dijkstra(source, graph, distances);
-    
-    cout << "Shortest distances from source " << source << ":\n";
-    for (int i = 0; i < n; ++i) {
-        cout << "Vertex " << i << " : " << distances[i] << "\n";
-    }
-    
+
+    network.divideAndConquer(0, vertices - 1);
+
     return 0;
 }
+
